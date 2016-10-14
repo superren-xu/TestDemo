3bbc08060de85bac795268bac7c2aa8ed2f9981b
superren <xyhailzl@gmail.com>
2016-10-14 12:36:22 +0800

    update

--- app/src/main/java/qianfeng/com/githubdemo/MainActivity.java
+++ app/src/main/java/qianfeng/com/githubdemo/MainActivity.java
@@ -2,12 +2,17 @@ package qianfeng.com.githubdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextView = ((TextView) findViewById(R.id.tv));
+        mTextView.setText("代码块修改了!!!");
     }
 }
